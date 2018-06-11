5e52d08425c00f931b452bfaa41f8fa55c3302f2
j.pavie <freudy31@gmail.com>
2018-06-11 16:47:33 +0200

    first outing implementation

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,14 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+
+import { AngularFontAwesomeModule  } from 'angular-font-awesome'
+
+import { ExperiencesComponent } from './pages/experiences/experiences.component'
+import { CompetencesComponent } from './pages/competences/competences.component'
+import { DiplomesComponent } from './pages/diplomes/diplomes.component'
+import { DiversComponent } from './pages/divers/divers.component'
+
+import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExperiencesComponent,
+    CompetencesComponent,
+    DiplomesComponent,
+    DiversComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
